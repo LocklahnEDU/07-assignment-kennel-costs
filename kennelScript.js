6dc4971cfc8a622cb0c89c5a4af9cb753b041898
LocklahnEDU <locklahnfrancis@mail.weber.edu>
2024-10-17 16:51:50 -0600

    Done

---
+++ kennelScript.js
@@ -0,0 +1,8 @@
+
+function CalculateKennel(){
+    let dogSize = document.getElementById("dogSize").value;
+    let numDays = document.getElementById("numDays").value;
+    let beforeTax = parseInt(dogSize) * numDays;
+    let totalCost = Math.round(100 * (beforeTax + (beforeTax * 0.0485))) / 100;
+    document.getElementById("answer").textContent = `Cost: $${totalCost}`;
+}
